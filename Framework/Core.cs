ea16b1fe7e9c68a2a06e1af2a5b854031f820391
hatim oura <147079928+ourabigdev@users.noreply.github.com>
2025-01-06 00:06:50 +0100

    implementing Core and Color class

---
+++ Framework/Core.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmlilCSharp
+{
+    public class Core
+    {
+        [DllImport("Amlil", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void SetFps(int FPS);
+    }
+}
